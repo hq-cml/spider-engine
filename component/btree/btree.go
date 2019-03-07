927c25f37d7e87862720ac4906669641636925fe
hq <hq_cml@163.com>
2019-03-07 20:38:22 +0800

    B+Tree接口

---
+++ component/btree/btree.go
@@ -0,0 +1,12 @@
+package btree
+
+type Btree interface {
+	AddBTree(treeName string)
+	Set(treeName, key string, value uint64) error
+	MutiSet(treeName string, kv map[string]string) error
+	GetInt(treeName, key string) (int64, bool)
+	Inc(treeName, key string)
+	GetFristKV(treeName string) (string, uint32, uint32, int, bool)
+	GetNextKV(treeName, key string) (string, uint32, uint32, int, bool)
+	Close()
+}
