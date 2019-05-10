19b8af01bbc82078425d6cacf512ed8a8f1b2302
hq <hq_cml@163.com>
2019-05-10 12:35:00 +0800

    自动安装脚本

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+spider_path=$SPIDER_PATH
+
+#校验
+if [ -d $spider_path ]; then
+    echo "The path $spider_path already exist!"
+    exit
+fi
+
+#目录创建
+mkdir $spider_path 
+mkdir $spider_path/log 
+mkdir $spider_path/data
+mkdir $spider_path/conf
+
+#文本替换
+sed "s#TODO_REPLACE#$spider_path#" ./conf/spider.conf > $spider_path/conf/spider.conf
+
+#执行文件copy
+cp ./spider-engine $spider_path
