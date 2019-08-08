8c0fd5624e9503b04f670b17e64ea4295c5f7650
hq <hq_cml@163.com>
2019-08-08 23:09:16 +0800

    json

--- utils/helper/json.go
+++ utils/helper/json.go
@@ -1,20 +1,28 @@
 package helper
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 func JsonEncode(d interface{}) string {
+	if d == nil {
+		return ""
+	}
 	s, e := json.Marshal(d)
 	if e != nil {
-		return e.Error()
+		return ""
 	} else {
 		return string(s)
 	}
 }
 
 func JsonEncodeIndent(d interface{}) string {
+	if d == nil {
+		return ""
+	}
 	s, e := json.MarshalIndent(d, "", "  ")
 	if e != nil {
-		return e.Error()
+		return ""
 	} else {
 		return string(s)
 	}
