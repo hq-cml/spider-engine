78d4be8937e0cb45774f252b598fd2cfb3991f9d
hq <hq_cml@163.com>
2019-11-19 21:49:07 +0800

    压缩，缩小DocNode

--- core/index/common.go
+++ core/index/common.go
@@ -1,29 +1,28 @@
 package index
 
 import (
-	"github.com/hq-cml/spider-engine/splitter"
 	"github.com/hq-cml/spider-engine/basic"
+	"github.com/hq-cml/spider-engine/splitter"
 	"strings"
 )
 
 // 索引类型说明
 const (
-	IDX_TYPE_PK            = 101 //主键类型，特殊，primarykey和docid一一映射，正排和倒排都维护在一颗独立的B+树中
+	IDX_TYPE_PK = 101 //主键类型，特殊，primarykey和docid一一映射，正排和倒排都维护在一颗独立的B+树中
 
-	IDX_TYPE_STR_WHOLE     = 201 //字符型索引, 全词匹配，倒排搜索，用于姓名等字段
-	IDX_TYPE_STR_SPLITER   = 202 //字符型索引, 切词匹配，倒排搜索，用于全文索引
-	IDX_TYPE_STR_LIST      = 203 //字符型索引, 列表类型，倒排搜索，分号切词
-	IDX_TYPE_STR_WORD      = 204 //字符型索引, 单字切词，倒排搜索，自然语言单个字母或者汉字
+	IDX_TYPE_STR_WHOLE   = 201 //字符型索引, 全词匹配，倒排搜索，用于姓名等字段
+	IDX_TYPE_STR_SPLITER = 202 //字符型索引, 切词匹配，倒排搜索，用于全文索引
+	IDX_TYPE_STR_LIST    = 203 //字符型索引, 列表类型，倒排搜索，分号切词
+	IDX_TYPE_STR_WORD    = 204 //字符型索引, 单字切词，倒排搜索，自然语言单个字母或者汉字
 
-	IDX_TYPE_INTEGER       = 301 //数字型索引，只支持整数，数字型索引只建立正排
+	IDX_TYPE_INTEGER = 301 //数字型索引，只支持整数，数字型索引只建立正排
 
-	IDX_TYPE_DATE          = 401 //日期型索引，数字类型的变种，只建立正排，转成时间戳存储
+	IDX_TYPE_DATE = 401 //日期型索引，数字类型的变种，只建立正排，转成时间戳存储
 
-	IDX_TYPE_GOD           = 501 //上帝视角索引, 特殊隐藏字段用于跨字段搜索, 只有倒排索引, 无正排
+	IDX_TYPE_GOD = 501 //上帝视角索引, 特殊隐藏字段用于跨字段搜索, 只有倒排索引, 无正排
 
-	IDX_TYPE_PURE_TEXT     = 601 //纯文本字符类型, 只保存详情，不参与检索, 只有正排没有倒排
+	IDX_TYPE_PURE_TEXT = 601 //纯文本字符类型, 只保存详情，不参与检索, 只有正排没有倒排
 )
-
 
 const (
 	IDX_TYPE_NAME_PRIME = "primary"
@@ -34,44 +33,44 @@ const (
 	IDX_TYPE_NAME_INT   = "number"
 )
 
-var IDX_MAP = map[string]uint16 {
-	IDX_TYPE_NAME_PRIME : IDX_TYPE_PK,
-	IDX_TYPE_NAME_WHOLE : IDX_TYPE_STR_WHOLE,
-	IDX_TYPE_NAME_WORDS : IDX_TYPE_STR_SPLITER,
-	IDX_TYPE_NAME_PURE  : IDX_TYPE_PURE_TEXT,
-	IDX_TYPE_NAME_TIME  : IDX_TYPE_DATE,
-	IDX_TYPE_NAME_INT   : IDX_TYPE_INTEGER,
+var IDX_MAP = map[string]uint16{
+	IDX_TYPE_NAME_PRIME: IDX_TYPE_PK,
+	IDX_TYPE_NAME_WHOLE: IDX_TYPE_STR_WHOLE,
+	IDX_TYPE_NAME_WORDS: IDX_TYPE_STR_SPLITER,
+	IDX_TYPE_NAME_PURE:  IDX_TYPE_PURE_TEXT,
+	IDX_TYPE_NAME_TIME:  IDX_TYPE_DATE,
+	IDX_TYPE_NAME_INT:   IDX_TYPE_INTEGER,
 }
 
-var RE_IDX_MAP = map[uint16]string {
-	IDX_TYPE_PK : IDX_TYPE_NAME_PRIME,
-	IDX_TYPE_STR_WHOLE : IDX_TYPE_NAME_WHOLE,
-	IDX_TYPE_STR_SPLITER : IDX_TYPE_NAME_WORDS,
-	IDX_TYPE_PURE_TEXT : IDX_TYPE_NAME_PURE,
-	IDX_TYPE_DATE : IDX_TYPE_NAME_TIME,
-	IDX_TYPE_INTEGER : IDX_TYPE_NAME_INT,
+var RE_IDX_MAP = map[uint16]string{
+	IDX_TYPE_PK:          IDX_TYPE_NAME_PRIME,
+	IDX_TYPE_STR_WHOLE:   IDX_TYPE_NAME_WHOLE,
+	IDX_TYPE_STR_SPLITER: IDX_TYPE_NAME_WORDS,
+	IDX_TYPE_PURE_TEXT:   IDX_TYPE_NAME_PURE,
+	IDX_TYPE_DATE:        IDX_TYPE_NAME_TIME,
+	IDX_TYPE_INTEGER:     IDX_TYPE_NAME_INT,
 }
 
 //全局分词器
 var Splitter splitter.Splitter
 
-var punctuationMap = map[string]bool {
-	" ":true,
-	".":true, "。":true,
-	",":true, "，":true,
-	"\\":true, "、":true,
-	";":true, "；":true,
-	":":true, "：":true,
-	"?":true, "？":true,
-	"!":true, "！":true,
-	"\"":true, "“":true, "”":true,
-	"(":true, ")":true,"（":true, "）":true,
-	"《":true, "》":true,
-	"-":true, "—":true,"":true,
-	"~":true, "#":true, "$":true,
-	"%":true, "&":true, "`":true, "^":true,
-	"+":true, "=":true,
-	"【":true, "】":true,
+var punctuationMap = map[string]bool{
+	" ": true,
+	".": true, "。": true,
+	",": true, "，": true,
+	"\\": true, "、": true,
+	";": true, "；": true,
+	":": true, "：": true,
+	"?": true, "？": true,
+	"!": true, "！": true,
+	"\"": true, "“": true, "”": true,
+	"(": true, ")": true, "（": true, "）": true,
+	"《": true, "》": true,
+	"-": true, "—": true, "": true,
+	"~": true, "#": true, "$": true,
+	"%": true, "&": true, "`": true, "^": true,
+	"+": true, "=": true,
+	"【": true, "】": true,
 }
 
 const (
@@ -85,8 +84,8 @@ func init() {
 //全词分词
 func SplitWholeWords(docId uint32, content string) map[string]basic.DocNode {
 
-	m := map[string]basic.DocNode {
-		content: basic.DocNode {
+	m := map[string]basic.DocNode{
+		content: basic.DocNode{
 			DocId: docId,
 		},
 	}
@@ -96,9 +95,9 @@ func SplitWholeWords(docId uint32, content string) map[string]basic.DocNode {
 //分号分词
 func SplitSemicolonWords(docId uint32, content string) map[string]basic.DocNode {
 	terms := strings.Split(content, ";")
-	m := map[string]basic.DocNode {}
+	m := map[string]basic.DocNode{}
 	for _, term := range terms {
-		node := basic.DocNode {
+		node := basic.DocNode{
 			DocId: docId,
 		}
 		m[term] = node
@@ -110,13 +109,13 @@ func SplitSemicolonWords(docId uint32, content string) map[string]basic.DocNode 
 func SplitRuneWords(docId uint32, content string) map[string]basic.DocNode {
 	rstr := []rune(content)
 	uniqMap := make(map[rune]bool)
-	for _, r := range rstr {   //去重
+	for _, r := range rstr { //去重
 		uniqMap[r] = true
 	}
 
-	m := map[string]basic.DocNode {}
+	m := map[string]basic.DocNode{}
 	for term := range uniqMap {
-		node := basic.DocNode {
+		node := basic.DocNode{
 			DocId:  docId,
 			Weight: 0,
 		}
@@ -128,7 +127,7 @@ func SplitRuneWords(docId uint32, content string) map[string]basic.DocNode {
 //真分词, 利用分词器, 同时, 计算词频TF
 func SplitTrueWords(docId uint32, content string) map[string]basic.DocNode {
 
-	terms :=  Splitter.DoSplit(content, false) //TODO true config
+	terms := Splitter.DoSplit(content, false)
 
 	terms = trimPunctuation(terms) //过滤无意义的标点
 	totalCnt := len(terms)
@@ -139,14 +138,14 @@ func SplitTrueWords(docId uint32, content string) map[string]basic.DocNode {
 		if _, ok := uniqMap[term]; !ok {
 			uniqMap[term] = 1
 		} else {
-			uniqMap[term] ++
+			uniqMap[term]++
 		}
 	}
-	m := map[string]basic.DocNode {}
+	m := map[string]basic.DocNode{}
 	for term, termCnt := range uniqMap {
-		node := basic.DocNode {
+		node := basic.DocNode{
 			DocId:  docId,
-			Weight: uint32((float32(termCnt)/float32(totalCnt)) * BIGGER_MULTIPLE), //词频放大10000倍
+			Weight: uint16((float32(termCnt) / float32(totalCnt)) * BIGGER_MULTIPLE), //词频放大10000倍
 		}
 		m[term] = node
 	}
